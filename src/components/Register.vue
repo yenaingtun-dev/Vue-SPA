43a8b1c9b3e77c3d1b50a63d89b424abae496892
YeNaingTun <yenaingtun425@gmail.com>
2024-03-18 09:45:45 +0630

    add loading

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -2,10 +2,13 @@
     <div class="mx-auto w-full px-4 py-16 sm:px-6 lg:px-8">
         <div class="mx-auto max-w-lg">
             <h1 class="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">Register Form</h1>
+            <div v-if="networkError" class="rounded border-s-4 border-red-500 bg-red-50 p-4 mt-4">
+                <strong class="text-sm font-medium text-red-800"> {{ networkError }} </strong>
+            </div>
             <div v-if="success" class="rounded border-s-4 border-green-500 bg-green-50 p-4 mt-4">
                 <strong class="block font-medium text-green-800"> {{ success }} </strong>
             </div>
-            <RegisterForm @register="register" :errors="errors"></RegisterForm>
+            <RegisterForm @register="register" :errors="errors" :loading="loading"></RegisterForm>
         </div>
     </div>
 </template>
@@ -17,6 +20,8 @@ export default {
     data() {
         return {
             success: '',
+            networkError: '',
+            loading: false,
             errors: {}
         }
     },
@@ -25,6 +30,7 @@ export default {
     },
     methods: {
         async register(user) {
+            this.loading = true;
             try {
                 let response = await axios.post(`http://127.0.0.1:8000/api/register`, {
                     name: user.name,
@@ -41,7 +47,13 @@ export default {
                 this.errors.message = '';
                 this.$router.push('/')
             } catch (e) {
-                this.errors = e.response.data.errors;
+                if (typeof e.response !== 'undefined') {
+                    this.errors = e.response.data.errors;
+                } else {
+                    this.networkError = e.message;
+                }
+            } finally {
+                this.loading = false;
             }
         },
     },
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -2,10 +2,13 @@
   <div class="mx-auto w-full px-4 py-16 sm:px-6 lg:px-8">
     <div class="mx-auto max-w-lg">
       <h1 class="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">Sign In Form</h1>
+      <div v-if="networkError" class="rounded border-s-4 border-red-500 bg-red-50 p-4 mt-4">
+        <strong class="text-sm font-medium text-red-800"> {{ networkError }} </strong>
+      </div>
       <div v-if="success" class="rounded border-s-4 border-green-500 bg-green-50 p-4 mt-4">
         <strong class="block font-medium text-green-800"> {{ success }} </strong>
       </div>
-      <SignInForm @signIn="signIn" :errors="errors" :message="message"></SignInForm>
+      <SignInForm @signIn="signIn" :errors="errors" :message="message" :loading="loading"></SignInForm>
     </div>
   </div>
 </template>
@@ -18,6 +21,8 @@ export default {
     return {
       success: '',
       message: '',
+      networkError: '',
+      loading: false,
       errors: {}
     }
   },
@@ -26,6 +31,7 @@ export default {
   },
   methods: {
     async signIn(user) {
+      this.loading = true;
       try {
         let response = await axios.post(`http://127.0.0.1:8000/api/login`, {
           email: user.email,
@@ -38,8 +44,14 @@ export default {
         this.errors.message = '';
         this.$router.push('/')
       } catch (e) {
-        this.errors = e.response.data.errors;
-        this.message = e.response.data.message;
+        if (typeof e.response !== 'undefined') {
+          this.errors = e.response.data.errors;
+          this.message = e.response.data.message;
+        } else {
+          this.networkError = e.message;
+        }
+      } finally {
+        this.loading = false;
       }
     },
   },
