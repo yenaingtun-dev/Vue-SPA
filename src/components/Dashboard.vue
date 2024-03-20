7d27c2cafe6f9265b454eb7cb8894b2ba86feb7b
YeNaingTun <yenaingtun425@gmail.com>
2024-03-20 09:47:51 +0630

    updated post loading

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,34 +2,54 @@
     <div class="mx-auto w-full px-4 py-16 sm:px-6 lg:px-8">
         <div class="mx-auto max-w-lg">
             <h1 class="text-center text-2xl font-bold text-teal-500 mb-3 sm:text-3xl">Dashboard Posts</h1>
-            <Posts :posts="posts"></Posts>
+            <svg v-if="loading" class="animate-spin w-10 h-8 mx-auto loading-svg" version="1.1" id="L9"
+                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
+                <path fill="black"
+                    d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+                </path>
+            </svg>
+            <Posts v-if="!loading" :posts="posts"></Posts>
         </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Posts from './Posts.vue';
 export default {
+    components: {
+        'Posts': Posts
+    },
     data() {
         return {
             posts: [],
             token: localStorage.getItem('bearerToken'),
+            loading: false,
         }
     },
     mounted() {
-        try {
-            const config = {
-                headers: { Authorization: `Bearer ${this.token}`, proxy: false }
-            };
-            axios.get(`http://127.0.0.1:8000/api/posts`, config)
-                .then(response => {
-                    this.posts = Object.values(response.data.data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        } catch (e) {
-            this.errors = e.response.data.errors;
+        this.getPosts();
+    },
+    methods: {
+        async getPosts() {
+            this.loading = true;
+            try {
+                const config = {
+                    headers: { Authorization: `Bearer ${this.token}`, proxy: false }
+                };
+                await axios.get(`http://127.0.0.1:8000/api/posts`, config)
+                    .then(response => {
+                        this.posts = Object.values(response.data.data);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            } catch (e) {
+                this.errors = e.response.data.errors;
+            } finally {
+                this.loading = false;
+            }
         }
     }
 }
--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,22 +1,20 @@
 <template>
     <div class="mb-2" v-for="post in posts">
-        <article class="rounded-lg border border-gray-300 bg-white p-4 shadow-sm transition hover:shadow-lg sm:p-6">
-            <a href="#">
-                <h3 class="mt-0.5 text-lg font-medium text-gray-900">
-                    {{ post.title }}
-                </h3>
-            </a>
+        <div class="rounded-lg border border-gray-300 bg-white p-4 shadow-sm transition hover:shadow-lg sm:p-6">
+            <h3 class="mt-0.5 text-lg font-medium text-gray-900">
+                {{ post.title }}
+            </h3>
             <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
                 {{ post.description }}
             </p>
-        </article>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     props: {
-        posts: Object;
+        posts: Object,
     },
 }
 </script>
